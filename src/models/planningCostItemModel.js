cf3edc3f9c867ffa2d5c6a37fbc371934e20e6a7
V-Perotto <vittorio.perotto@outlook.com>
2023-08-11 19:19:19 -0300

    Alterando nome do model de planejamento de custo para PlanningCostItemModel

--- src/models/planningCostItemModel.js
+++ src/models/planningCostItemModel.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const PlanningCostSchema = new mongoose.Schema({
+const PlanningCostItemSchema = new mongoose.Schema({
     process : { type: mongoose.Schema.Types.ObjectId, ref: "Process", required: true },
     title: { type: String, required: true },
     value: { type: Number, required: true },
 }, { timestamps: {} });
 
-module.exports = mongoose.model('PlanningCost', PlanningCostSchema);
+module.exports = mongoose.model('PlanningCostItem', PlanningCostItemSchema);
