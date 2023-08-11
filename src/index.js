605667da808e1e6ef0ccbf9d9c3b7f07c50ad886
V-Perotto <vittorio.perotto@outlook.com>
2023-08-11 15:15:58 -0300

    Adicionando configurações do Swagger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 require("dotenv/config");
 const cors = require('cors');
 const routes = require('./routes');
+const SwaggerUI = require('swagger-ui-express');
+const SwaggerFile = require('./swagger-output.json');
 
 const app = express();
 // mongoose.set('useCreateIndex', true);
@@ -12,7 +14,9 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true });
 
 const db = mongoose.connection
 db.on('error', (error) => console.error(error));
-db.once('open', () => console.log("Connected to Database"))
+db.once('open', () => console.log("Connected to Database"));
+
+app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(SwaggerFile));
 
 app.use(cors());
 app.use(express.json());
