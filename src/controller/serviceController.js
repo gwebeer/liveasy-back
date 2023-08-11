4a605f9fa08f2661b6a3655e433a3e9c8045c1b0
V-Perotto <vittorio.perotto@outlook.com>
2023-08-10 21:17:09 -0300

    Adicionando controller de Service

---
+++ src/controller/serviceController.js
@@ -0,0 +1,53 @@
+const Service = require("../models/serviceModel");
+
+module.exports = {
+ 
+    async createService(request, response) {
+        try {
+            const createService = await Service.create(request.body);
+            return response.status(201).json({ createService });
+        } catch (error) {
+            return response.status(400).json({ error: error });
+        }
+    },
+
+    async getService(request, response) {
+        try {
+            const getService = await Service.findOne({ _id: request.params.id })
+            return response.json(getService);
+        } catch (error) {
+            console.log("Erro ao obter o detalhe do serviço", error)
+            return response.status(400).json({ error });
+        }
+    },
+
+    async getServices(request, response) {
+        try {
+            const getServices = await Service.find()
+            return response.json(getServices);
+        } catch (error) {
+            console.log("Erro ao obter todos os detalhes de serviços", error)
+            return response.status(400).json({ error });
+        }
+    },
+    
+    async updateService(request, response) {
+        try {
+            const updateService = await Service.findOneAndUpdate({ user: request.params.value }, request.body);
+            return response.json(updateService);
+        } catch (error) {
+            console.log("Erro ao atualizar o serviço da coleção Service", error);
+            return response.status(400).json({ error });
+        }
+    },
+    
+    async deleteService(request, response) {
+        try {
+            const deleteService = await Service.delete({ _id: request.params.id})
+            return response.json(deleteService)
+        } catch (error) {
+            console.log("Erro ao deletar o serviço da coleção Service", error);
+        }
+    },
+    
+}
