ce7ae207721a09012075d2a6a5db3ff34f734442
V-Perotto <vittorio.perotto@outlook.com>
2023-08-10 21:30:18 -0300

    Alterando nomes em português para inglês

--- src/models/processModel.js
+++ src/models/processModel.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
-// Informações de Consumos
 const ProcessSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
     step: { type: String, required: true },
     status: { type: String, required: true },
-    renda_mensal: { type: String, required: false },
-    orcamento_especial: { type: String, required: false },
-    data_mudanca: { type: Date, required: false },
-    
+    monthly_income: { type: String, required: false },
+    special_budget: { type: String, required: false },
+    moving_date: { type: Date, required: false },
 }, { timestamps: {} });
 
 module.exports = mongoose.model('Moves', ProcessSchema);
