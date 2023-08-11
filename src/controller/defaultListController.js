28cc3d561abf4894b92f24e1d841930bed3ff435
V-Perotto <vittorio.perotto@outlook.com>
2023-08-10 21:22:29 -0300

    Refatorando código e separando função de get (getItem e getItems)

--- src/controller/defaultListController.js
+++ src/controller/defaultListController.js
@@ -1,57 +1,54 @@
 const DefaultList = require('../models/defaultListModel');
 
 module.exports = {
-    // Create Item
-    async createItem(req, res) {
+    
+    async createItem(request, response) {
         try {
-            const createItem = await DefaultList.create(req.body);
-            return res.json({ createItem });
+            const createItem = await DefaultList.create(request.body);
+            return response.json({ createItem });
         } catch (error) {
-            return res.status(400).json({ error: error });
+            return response.status(400).json({ error: error });
         }
     },
 
-    // Delete Item
-    async deleteItem(req, res) {
+    async getItem(request, response) {
         try {
-            const deleteItem = await DefaultList.findOneAndDelete({ _id: req.params.id });
-            return res.json(deleteItem);
+            const getItem = await DefaultList.findOne({ _id: request.params.id })
+            return response.json(getItem);
         } catch (error) {
-            console.log("Erro ao deletar item da coleção DefaultList", error)
-            return res.status(400).json({ error });
+            console.log("Erro ao obter o item requisitado", error)
+            return response.status(400).json({ error });
         }
     },
 
-    // Update Item
-    async updateItem(req, res) {
+    async getItems(request, response) {
         try {
-            const updateItem = await DefaultList.findOneAndUpdate({ _id: req.params.id }, req.body);
-            return res.json(updateItem);
+            const getItem = await DefaultList.find()
+            return response.json(getItem);
+        } catch (error) {
+            console.log("Erro ao obter todos os itens", error)
+            return response.status(400).json({ error });
+        }
+    },
+    
+    async updateItem(request, response) {
+        try {
+            const updateItem = await DefaultList.findOneAndUpdate({ _id: request.params.id }, request.body);
+            return response.json(updateItem);
         } catch (error) {
             console.log("Erro ao atualizar item da coleção DefaultList", error)
-            return res.status(400).json({ error });
+            return response.status(400).json({ error });
         }
     },
-
-    // Get Process(es)
-    async getItem(req, res) {
-        if (req.params.id == "all") {
-            try {
-                const getItem = await DefaultList.find()
-                return res.json(getItem);
-            } catch (error) {
-                console.log("Erro ao obter todos os itens", error)
-                return res.status(400).json({ error });
-            }
-        } else {
-            try {
-                const getItem = await DefaultList.findOne({ _id: req.params.id })
-                return res.json(getItem);
-            } catch (error) {
-                console.log("Erro ao obter o item requisitado", error)
-                return res.status(400).json({ error });
-            }
+    
+    async deleteItem(request, response) {
+        try {
+            const deleteItem = await DefaultList.findOneAndDelete({ _id: request.params.id });
+            return response.json(deleteItem);
+        } catch (error) {
+            console.log("Erro ao deletar item da coleção DefaultList", error)
+            return response.status(400).json({ error });
         }
-
-    }
+    },
+    
 }
