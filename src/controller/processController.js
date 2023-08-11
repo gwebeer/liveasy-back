9caf562ae17eedd412a1853f341679aaf08a07e8
V-Perotto <vittorio.perotto@outlook.com>
2023-08-10 21:23:07 -0300

    Refatorando código e separando função de get (getProcess e getProcesses)

--- src/controller/processController.js
+++ src/controller/processController.js
@@ -1,99 +1,84 @@
 const Process = require('../models/processModel');
 
 module.exports = {
-    // Create Process
-    async createProcess(req, res) {
+    
+    async createProcess(request, response) {
         try {
-            const createProcess = await Process.create(req.body);
-            return res.status(201).json({ createProcess });
+            const createProcess = await Process.create(request.body);
+            return response.status(201).json({ createProcess });
         } catch (error) {
-            return res.status(400).json({ error: error });
+            return response.status(400).json({ error: error });
         }
     },
-    // Create Process
-    async createDirect(content) {
+    
+    async getProcess(request, response) {
         try {
-            const createProcess = await Process.create(content);
-            return
-        } catch (error) {
-            return
-        }
-    },
-
-    // Delete Process
-    async deleteProcess(req, res) {
-        try {
-            const deleteProcess = await Process.findOneAndDelete({ _id: req.params.id });
-            return res.json(deleteProcess);
-        } catch (error) {
-            console.log("Erro ao deletar item da coleção Process", error)
-            return res.status(400).json({ error });
-        }
-    },
-
-    // Update Process
-    async updateProcess(req, res) {
-        if (req.params.key === "user") {
-            try {
-                const proccessUpdate = await Process.findOneAndUpdate({ user: req.params.value }, req.body);
-                return res.json(proccessUpdate);
-            } catch (error) {
-                console.log("Erro ao atualizar item da coleção Rooms", error)
-                return res.status(400).json({ error });
-            }
-        }
-
-        if (req.params.key === "id") {
-            try {
-                const proccessUpdate = await Process.findOneAndUpdate({ _id: req.params.value }, req.body);
-                return res.json(proccessUpdate);
-            } catch (error) {
-                console.log("Erro ao atualizar item da coleção Rooms", error)
-                return res.status(400).json({ error });
-            }
-        }
-
-
-
-        try {
-            const updateProcess = await Process.findOneAndUpdate({ _id: req.params.id }, req.body);
-            return res.json(updateProcess);
-        } catch (error) {
-            console.log("Erro ao atualizar item da coleção Process", error)
-            return res.status(400).json({ error });
-        }
-    },
-
-    // Get Process(es)
-    async getProcess(req, res) {
-        if (req.params.id == "all") {
-            try {
-                const getProcesses = await Process.find()
-                return res.json(getProcesses);
-            } catch (error) {
-                console.log("Erro ao obter todos os processos", error)
-                return res.status(400).json({ error });
-            }
-        } else {
-            try {
-                const getProcess = await Process.findOne({ _id: req.params.id })
-                return res.json(getProcess);
-            } catch (error) {
-                console.log("Erro ao obter o detalhe do processo", error)
-                return res.status(400).json({ error });
-            }
-        }
-
-    },
-
-    async getUserProcess(req, res) {
-        try {
-            const getProcess = await Process.findOne({ user: req.params.id })
-            return res.json(getProcess);
+            const getProcess = await Process.findOne({ _id: request.params.id })
+            return response.json(getProcess);
         } catch (error) {
             console.log("Erro ao obter o detalhe do processo", error)
-            return res.status(400).json({ error });
+            return response.status(400).json({ error });
         }
-    }
+    },
+    
+    async getProcesses(request, response) {
+        try {
+            const getProcesses = await Process.find()
+            return response.json(getProcesses);
+        } catch (error) {
+            console.log("Erro ao obter todos os processos", error)
+            return response.status(400).json({ error });
+        }
+    },
+    
+    async getUserProcess(request, response) {
+        try {
+            const getProcess = await Process.findOne({ user: request.params.id })
+            return response.json(getProcess);
+        } catch (error) {
+            console.log("Erro ao obter o detalhe do processo", error)
+            return response.status(400).json({ error });
+        }
+    },
+    
+    async updateProcess(request, response) {
+        if (request.params.key === "user") {
+            try {
+                const proccessUpdate = await Process.findOneAndUpdate({ user: request.params.value }, request.body);
+                return response.json(proccessUpdate);
+            } catch (error) {
+                console.log("Erro ao atualizar item da coleção Rooms", error)
+                return response.status(400).json({ error });
+            }
+        }
+
+        if (request.params.key === "id") {
+            try {
+                const proccessUpdate = await Process.findOneAndUpdate({ _id: request.params.value }, request.body);
+                return response.json(proccessUpdate);
+            } catch (error) {
+                console.log("Erro ao atualizar item da coleção Rooms", error)
+                return response.status(400).json({ error });
+            }
+        }
+
+        try {
+            const updateProcess = await Process.findOneAndUpdate({ _id: request.params.id }, request.body);
+            return response.json(updateProcess);
+        } catch (error) {
+            console.log("Erro ao atualizar item da coleção Process", error)
+            return response.status(400).json({ error });
+        }
+    },
+    
+    async deleteProcess(request, response) {
+        try {
+            const deleteProcess = await Process.findOneAndDelete({ _id: request.params.id });
+            return response.json(deleteProcess);
+        } catch (error) {
+            console.log("Erro ao deletar item da coleção Process", error)
+            return response.status(400).json({ error });
+        }
+    },
 
 }
