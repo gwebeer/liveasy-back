3c52e37304e7c80b933c0b07c91f9ad1b3b6aa50
V-Perotto <vittorio.perotto@outlook.com>
2023-08-10 21:29:40 -0300

    Alterando nome de PersonalList para ItemList

--- src/models/itemListModel.js
+++ src/models/itemListModel.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-// Informações de Consumos
-const PersonalListSchema = new mongoose.Schema({
-    item: { type: String, required: true },
+const ItemListSchema = new mongoose.Schema({
+    title: { type: String, required: true },
     priority: { type: String, required: true },
     category: { type: String, required: true },
+    convenient: { type: String, required: true },
     value: { type: String, required: true },
     bought: { type: Boolean, required: true },
     proccess: { type: mongoose.Schema.Types.ObjectId, ref: "moves", required: true },
 }, { timestamps: {} });
 
-module.exports = mongoose.model('personal-item', PersonalListSchema);
+module.exports = mongoose.model('item-list', ItemListSchema);
