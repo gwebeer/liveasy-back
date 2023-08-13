8ab78a23937b907ba1f24d4b5c01b3694cbe13fb
V-Perotto <vittorio.perotto@outlook.com>
2023-08-13 18:17:41 -0300

    Adicionando definitions (Models)

--- src/swagger.js
+++ src/swagger.js
@@ -7,6 +7,94 @@ const doc = {
   },
   host: 'localhost:4000',
   schemes: ['http'],
+  definitions: {
+    CalendarItemSchema: {
+      $user: {
+        $ref: '#/definitions/UserSchema'
+      },
+      $service: {
+        $ref: '#/definitions/ServiceSchema'
+      },
+      $initialDate: "2023-07-21",
+      finalDate: "2023-08-21",
+      $repeat: true,
+      repeatEvery: 30 
+    },
+    DashboardSchema: {
+      $process: {
+        $ref: '#/definitions/ProcessSchema'
+      },
+      $user: {
+        $ref: '#/definitions/UserSchema'
+      }
+    },
+    DefaultListSchema: {
+      $option: "Item",
+      $title: "Sofá",
+      category: "Móveis",
+      convenient: "Sala"
+    },
+    ItemSchema: {
+      $title: "Sofá",
+      $priority: "Média",
+      $category: "Móveis",
+      $convenient: "Sala",
+      $value: 2500.00,
+      $bought: false,
+      $process: {
+        $ref: '#/definitions/ProcessSchema'
+      }
+    },
+    OpinionAboutPlaceSchema: {
+      $place: {
+        $ref: '#/definitions/PlaceSchema'
+      },
+      $user: {
+        $ref: '#/definitions/UserSchema'
+      },
+      $pros: ["Lugar muito bom de morar", "Tem posto de combustível perto"],
+      $cons: ["Pega pouco sol", "Cheiro de mofo"]
+    },
+    PlaceSchema: {
+      $name: "Edifício Non Facile",
+      $address: "Rua dos Desafios, 352",
+      $zipCode: "70000-600",
+      $neighborhood: "Promessas",
+      $condominiumValue: 1200.00
+    },
+    PlanningCostItemSchema: {
+      $process: {
+        $ref: '#/definitions/ProcessSchema'
+      },
+      $title: "Aluguel",
+      $value: 850.00
+    },
+    ProcessSchema: {
+      $user: {
+        $ref: '#/definitions/UserSchema'
+      },
+      $step: "Planejamento",
+      $status: "Em progresso",
+      monthly_income: 2652.85,
+      special_budget: 8500.00,
+      moving_date: "2024-02-25"
+    },
+    ServiceSchema: {
+      $process: {
+        $ref: '#/definitions/ProcessSchema'
+      },
+      $title: "Instalação de Internet",
+      $value: 120.00
+    },
+    UserSchema: {
+      $name: "Irineu de Souza",
+      $email: "irineu@naosabe.com",
+      $birthDate: "2001-01-14",
+      $password: "Minh@S3nh4",
+      $type: "customer",
+      $phone: "41999999999"
+    }
+  }
 };
 
 const outputFile = './src/swagger-output.json';
