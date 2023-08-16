a4fb8958d2a4a973b1b7231116ed9189a40647b8
V-Perotto <vittorio.perotto@outlook.com>
2023-08-16 16:24:45 -0300

    Adicionando retornos com status definidos e esqueleto de documentação para o retorno das funções

--- src/controller/serviceController.js
+++ src/controller/serviceController.js
@@ -6,6 +6,16 @@ module.exports = {
         /* 
             #swagger.tags = ["serviceController"]
             #swagger.description = "Função que cria um novo serviço"
+            #swagger.responses[201] = {
+                schema: {
+
+                }
+            }
+            #swagger.responses[400] = {
+                schema: {
+                    
+                }
+            }
         */
         try {
             const createService = await Service.create(request.body);
@@ -19,11 +29,21 @@ module.exports = {
         /* 
             #swagger.tags = ["serviceController"]
             #swagger.description = "Função que busca um ou vários serviços"
+            #swagger.responses[200] = {
+                schema: {
+
+                }
+            }
+            #swagger.responses[400] = {
+                schema: {
+                    
+                }
+            }
         */
         if (request.params.id == "all") {
             try {
                 const getService = await Service.findOne({ _id: request.params.id })
-                return response.json(getService);
+                return response.status(200).json(getService);
             } catch (error) {
                 console.log("Erro ao obter o detalhe do serviço", error)
                 return response.status(400).json({ error });
@@ -31,7 +51,7 @@ module.exports = {
         } else {
             try {
                 const getServices = await Service.find()
-                return response.json(getServices);
+                return response.status(200).json(getServices);
             } catch (error) {
                 console.log("Erro ao obter todos os detalhes de serviços", error)
                 return response.status(400).json({ error });
@@ -43,10 +63,20 @@ module.exports = {
         /* 
             #swagger.tags = ["serviceController"]
             #swagger.description = "Função que atualiza um serviço"
+            #swagger.responses[200] = {
+                schema: {
+
+                }
+            }
+            #swagger.responses[400] = {
+                schema: {
+                    
+                }
+            }
         */
         try {
             const updateService = await Service.findOneAndUpdate({ user: request.params.value }, request.body);
-            return response.json(updateService);
+            return response.status(200).json(updateService);
         } catch (error) {
             console.log("Erro ao atualizar o serviço da coleção Service", error);
             return response.status(400).json({ error });
@@ -57,12 +87,23 @@ module.exports = {
         /* 
             #swagger.tags = ["serviceController"]
             #swagger.description = "Função que deleta um serviço"
+            #swagger.responses[200] = {
+                schema: {
+
+                }
+            }
+            #swagger.responses[400] = {
+                schema: {
+                    
+                }
+            }
         */
         try {
             const deleteService = await Service.delete({ _id: request.params.id})
-            return response.json(deleteService)
+            return response.status(200).json(deleteService)
         } catch (error) {
             console.log("Erro ao deletar o serviço da coleção Service", error);
+            return response.status(400).json({ error })
         }
     },
     
