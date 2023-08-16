c8c9f0643afa639b706d8ab275cc08a30342cab9
V-Perotto <vittorio.perotto@outlook.com>
2023-08-16 16:17:03 -0300

    Editando chaves para indicar que buscam pelo ObjectId dos Schemas que antes estavam definidos

--- src/swagger.js
+++ src/swagger.js
@@ -9,24 +9,16 @@ const doc = {
   schemes: ['http'],
   definitions: {
     CalendarItemSchema: {
-      $user: {
-        $ref: '#/definitions/UserSchema'
-      },
-      $service: {
-        $ref: '#/definitions/ServiceSchema'
-      },
+      $user: 'ObjectId()',
+      $service: 'ObjectId()',
       $initialDate: "2023-07-21",
       finalDate: "2023-08-21",
       $repeat: true,
       repeatEvery: 30 
     },
     DashboardSchema: {
-      $process: {
-        $ref: '#/definitions/ProcessSchema'
-      },
-      $user: {
-        $ref: '#/definitions/UserSchema'
-      }
+      $process: 'ObjectId()',
+      $user: 'ObjectId()'
     },
     DefaultListSchema: {
       $option: "Item",
@@ -41,17 +33,11 @@ const doc = {
       $convenient: "Sala",
       $value: 2500.00,
       $bought: false,
-      $process: {
-        $ref: '#/definitions/ProcessSchema'
-      }
+      $process: 'ObjectId()'
     },
     OpinionAboutPlaceSchema: {
-      $place: {
-        $ref: '#/definitions/PlaceSchema'
-      },
-      $user: {
-        $ref: '#/definitions/UserSchema'
-      },
+      $place: 'ObjectId()',
+      $user: 'ObjectId()',
       $pros: ["Lugar muito bom de morar", "Tem posto de combustível perto"],
       $cons: ["Pega pouco sol", "Cheiro de mofo"]
     },
@@ -63,16 +49,12 @@ const doc = {
       $condominiumValue: 1200.00
     },
     PlanningCostItemSchema: {
-      $process: {
-        $ref: '#/definitions/ProcessSchema'
-      },
+      $process: 'ObjectId()',
       $title: "Aluguel",
       $value: 850.00
     },
     ProcessSchema: {
-      $user: {
-        $ref: '#/definitions/UserSchema'
-      },
+      $user: 'ObjectId()',
       $step: "Planejamento",
       $status: "Em progresso",
       monthly_income: 2652.85,
@@ -80,9 +62,7 @@ const doc = {
       moving_date: "2024-02-25"
     },
     ServiceSchema: {
-      $process: {
-        $ref: '#/definitions/ProcessSchema'
-      },
+      $process: 'ObjectId()',
       $title: "Instalação de Internet",
       $value: 120.00
     },
