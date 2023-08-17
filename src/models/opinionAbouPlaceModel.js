1dd3876952ed4a8f88b1646ab6b86c582e98203c
V-Perotto <vittorio.perotto@outlook.com>
2023-08-17 16:21:46 -0300

    Corrigindo type para Array

--- src/models/opinionAbouPlaceModel.js
+++ src/models/opinionAbouPlaceModel.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const OpinionAboutPlaceSchema = new mongoose.Schema({
     place: { type: mongoose.Schema.Types.ObjectId, ref: "Places", required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
-    pros: { type: String, required: true },
-    cons: { type: String, required: true },
+    pros: { type: Array, required: true },
+    cons: { type: Array, required: true },
 }, { timestamps: {} });
 
 module.exports = mongoose.model("OpinionAboutPlace", OpinionAboutPlaceSchema);
