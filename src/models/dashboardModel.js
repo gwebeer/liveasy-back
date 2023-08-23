4027e065f4ec6d630166713afcd6191d716b1262
V-Perotto <vittorio.perotto@outlook.com>
2023-08-23 11:47:28 -0300

    Removendo 'process' e adicionando 'choosedProperty'

--- src/models/dashboardModel.js
+++ src/models/dashboardModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const DashboardSchema = new mongoose.Schema({
-    process: { type: mongoose.Schema.Types.ObjectId, ref: "Process", required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true }
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    choosedProperty: { type: mongoose.Schema.Types.ObjectId, ref: "ChoosedProperty", required: true }
 }, { timestamps: {} });
 
 module.exports = mongoose.model("Dashboard", DashboardSchema)
