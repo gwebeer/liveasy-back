645c81db5563ac101d89b498cab66a6869b05a48
V-Perotto <vittorio.perotto@outlook.com>
2023-08-23 11:33:29 -0300

    Renomeando e editando Controller de sugestão de item [DefaultList > SuggestionItem]

--- src/controller/suggestionItemController.js
+++ src/controller/suggestionItemController.js
@@ -1,16 +1,16 @@
-const DefaultList = require('../models/defaultListModel');
+const SuggestionItem = require('../models/suggestionItemModel');
 
 module.exports = {
     
-    async createItem(request, response) {
+    async createSuggestionItem(request, response) {
         /* 
-            #swagger.tags = ["defaultListController"]
+            #swagger.tags = ["suggestionItemController"]
             #swagger.description = "Função que cria um novo item padrão"
             #swagger.parameters['obj'] = {
                 in: 'body',
                 required: true,
                 schema: { 
-                    "$ref": "#/definitions/DefaultListSchema"
+                    "$ref": "#/definitions/SuggestionItemSchema"
                 }
             }
             #swagger.responses[201] = {
@@ -37,16 +37,16 @@ module.exports = {
             }
         */
         try {
-            const createItem = await DefaultList.create(request.body);
-            return response.status(201).json(createItem);
+            const createSuggestionItem = await SuggestionItem.create(request.body);
+            return response.status(201).json(createSuggestionItem);
         } catch (error) {
             return response.status(400).json({ error });
         }
     },
 
-    async getItem(request, response) {
+    async getSuggestionItem(request, response) {
         /* 
-            #swagger.tags = ["defaultListController"]
+            #swagger.tags = ["suggestionItemController"]
             #swagger.description = "Função que busca por um ou vários itens padrão"
             #swagger.responses[200] = {
                 schema: {
@@ -77,16 +77,16 @@ module.exports = {
         */
         if (request.params.id == "all") {
             try {
-                const getItem = await DefaultList.find()
-                return response.status(200).json(getItem);
+                const getSuggestionItems = await SuggestionItem.find()
+                return response.status(200).json(getSuggestionItems);
             } catch (error) {
                 console.log("Erro ao obter todos os itens", error)
                 return response.status(400).json({ error });
             }
         } else {
             try {
-                const getItem = await DefaultList.findOne({ _id: request.params.id })
-                return response.status(200).json(getItem);
+                const getSuggestionItem = await SuggestionItem.findOne({ _id: request.params.id })
+                return response.status(200).json(getSuggestionItem);
             } catch (error) {
                 console.log("Erro ao obter o item requisitado", error)
                 return response.status(400).json({ error });
@@ -94,9 +94,9 @@ module.exports = {
         } 
     },
     
-    async updateItem(request, response) {
+    async updateSuggestionItem(request, response) {
         /* 
-            #swagger.tags = ["defaultListController"]
+            #swagger.tags = ["suggestionItemController"]
             #swagger.description = "Função que atualiza um item padrão"
             #swagger.parameters['obj'] = {
                 in: 'body',
@@ -126,17 +126,17 @@ module.exports = {
             }
         */
         try {
-            const updateItem = await DefaultList.findOneAndUpdate({ _id: request.params.id }, request.body);
-            return response.status(200).json(updateItem);
+            const updateSuggestionItem = await SuggestionItem.findOneAndUpdate({ _id: request.params.id }, request.body);
+            return response.status(200).json(updateSuggestionItem);
         } catch (error) {
-            console.log("Erro ao atualizar item da coleção DefaultList", error)
+            console.log("Erro ao atualizar item da coleção SuggestionItem", error)
             return response.status(400).json({ error });
         }
     },
     
     async deleteItem(request, response) {
         /* 
-            #swagger.tags = ["defaultListController"]
+            #swagger.tags = ["suggestionItemController"]
             #swagger.description = "Função que deleta um item padrão"
             #swagger.responses[200] = {
                 schema: {
@@ -166,10 +166,10 @@ module.exports = {
             }
         */
         try {
-            const deleteItem = await DefaultList.findOneAndDelete({ _id: request.params.id });
+            const deleteItem = await SuggestionItem.findOneAndDelete({ _id: request.params.id });
             return response.status(200).json(deleteItem);
         } catch (error) {
-            console.log("Erro ao deletar item da coleção DefaultList", error)
+            console.log("Erro ao deletar item da coleção SuggestionItem", error)
             return response.status(400).json({ error });
         }
     },
