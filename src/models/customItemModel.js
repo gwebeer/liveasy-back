8313f460b8a8c23782800067997c765a23a117b9
V-Perotto <vittorio.perotto@outlook.com>
2023-08-23 11:37:05 -0300

    Renomeando e editando Model de Item Personalizado [Item > CustomItem]

---
+++ src/models/customItemModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const CustomItemSchema = new mongoose.Schema({
+    process: { type: mongoose.Schema.Types.ObjectId, ref: "Process", required: true },
+    convenient: { type: String, required: true },
+    title: { type: String, required: true },
+    priority: { type: String, required: true },
+    value: { type: String, required: true },
+    bought: { type: Boolean, required: true },
+    valuePaid: { type: Boolean, required: false },
+    boughtDate: { type: Boolean, required: false }
+}, { timestamps: {} });
+
+module.exports = mongoose.model('CustomItem', CustomItemSchema);
