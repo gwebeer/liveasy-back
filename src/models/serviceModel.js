079f041f5fce392023043e58a3dc6f0b63568bbc
V-Perotto <vittorio.perotto@outlook.com>
2023-08-23 11:38:51 -0300

    Removendo Controllers e Models que não fazem mais parte do escopo [OpinionAboutPlace e Service]

--- src/models/serviceModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ServiceSchema = new mongoose.Schema({
-    process : { type: mongoose.Schema.Types.ObjectId, ref: "Process", required: true },
-    title: { type: String, required: true },
-    value: { type: Number, required: true },
-}, { timestamps: {} });
-
-module.exports = mongoose.model('Service', ServiceSchema);
