8e0108e9e64d894b5f9aea8597d8158b3b7652c7
V-Perotto <vittorio.perotto@outlook.com>
2023-08-23 11:46:21 -0300

    Removendo objeto service e adicionando planningCostItem

--- src/models/calendarItemModel.js
+++ src/models/calendarItemModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const CalendarItemSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
-    service: { type: mongoose.Schema.Types.ObjectId, ref: "PlanningService", required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    planningCostItem: { type: mongoose.Schema.Types.ObjectId, ref: "PlanningCostItem", required: true },
     initialDate: { type: String, required: true },
     finalDate: { type: String, required: false },
     repeat: { type: Boolean, required: true },
