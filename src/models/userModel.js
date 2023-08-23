bba0912701a62495f338365b1c8988877a4bcf81
V-Perotto <vittorio.perotto@outlook.com>
2023-08-23 11:48:00 -0300

    Reorganizando 'User'

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,9 +4,9 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     birthDate: { type: String, required: true },
+    phone: { type: String, required: true},
     password: { type: String, required: true },
-    type: { type: String, required: true },
-    phone: { type: String, required: true}
+    type: { type: String, required: true }
 }, { timestamps: {} });
 
 module.exports = mongoose.model('User', UserSchema);
