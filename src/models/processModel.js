d7e9c76325e8afae568c44b82cb2fe2787f3e8dc
V-Perotto <vittorio.perotto@outlook.com>
2023-08-23 19:12:39 -0300

    Removendo informações do antigo escopo

--- src/models/processModel.js
+++ src/models/processModel.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const ProcessSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     step: { type: String, required: true },
-    status: { type: String, required: true },
-    monthly_income: { type: String, required: false },
-    special_budget: { type: String, required: false },
-    moving_date: { type: String, required: false },
+    status: { type: String, required: true }
 }, { timestamps: {} });
 
 module.exports = mongoose.model('Process', ProcessSchema);
