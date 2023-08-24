6d1ef13032fd295085de6cf124cfdde63a4f8f3f
V-Perotto <vittorio.perotto@outlook.com>
2023-08-24 20:55:33 -0300

    Adicionando 'category'

--- src/models/propertyItemModel.js
+++ src/models/propertyItemModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const PropertyItemSchema = new mongoose.Schema({
     property: { type: mongoose.Schema.Types.ObjectId, ref: "Property", required: true },
     title: { type: String, required: true },
+    category: { type: String, required: true },
     convenient: { type: String, required: true }
 }, { timestamps: {} });
 
--- src/models/suggestionItemModel.js
+++ src/models/suggestionItemModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const SuggestionItemSchema = new mongoose.Schema({
     type: { type: String, required: true },
     title: { type: String, required: true },
+    category: { type: String, required: false },
     convenient: { type: String, required: false }
 }, { timestamps: {} });
 
