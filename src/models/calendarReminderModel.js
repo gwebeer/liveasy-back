96336b01a9b47667b7f9d2ef3f9b5458222ebff0
V-Perotto <vittorio.perotto@outlook.com>
2023-08-24 20:53:08 -0300

    Alterando nome de objeto: planningCostItem > costItemList

--- src/models/calendarReminderModel.js
+++ src/models/calendarReminderModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const CalendarReminderSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    planningCostItem: { type: mongoose.Schema.Types.ObjectId, ref: "PlanningCostItem", required: true },
+    costItemList: { type: mongoose.Schema.Types.ObjectId, ref: "CostItemList", required: true },
     initialDate: { type: String, required: true },
     finalDate: { type: String, required: false },
     repeat: { type: Boolean, required: true },
