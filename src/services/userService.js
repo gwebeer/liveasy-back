1acb368523560b8359b5036d24b857c2091e6e01
V-Perotto <vittorio.perotto@outlook.com>
2023-08-28 10:04:27 -0300

    Adicionando funcionalidades para forgotPassword() e authenticateUser()

--- src/services/userService.js
+++ src/services/userService.js
@@ -67,12 +67,52 @@ export default class UserService {
     async authenticateUser(data) {
         const userModel = new UserModel();
         
-        
+        const authenticateUser = await userModel.findOne({ email: data.email })
+        if (!authenticateUser) {
+            return null;
+        } 
+        const checkPassword = await bcrypt.compare(data.password, authenticateUser.password);
+        if (!checkPassword) {
+            return false;
+        }
+        const secret = process.env.SECRET_TOKEN
+        const token = jwt.sign({
+            id: authenticateUser._id,
+            type: authenticateUser.type
+        }, secret)
+
+        return token;
     }
 
     async forgotPassword(data) {
         const userModel = new UserModel();
-        
+        const user = await userModel.findOne({ email: data.email });
+
+        if (user == null) {
+            return user;
+        } 
+        let mailtransporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "bochoskifelipe@gmail.com",
+                pass: "hcllrfvkxbzophcy",   
+            }
+        })
+
+        let details = {
+            from: "bochoskifelipe@gmail.com",
+            to: user.email,
+            subject: "Redefinição de senha",
+            html: `<p>Olá, você solicitou a redefinição de senha.</p>
+        <p>Clique <a href="http://localhost:3000/user/reset-password">aqui</a> para redefinir sua senha.</p>`,
+        }
+
+        mailtransporter.sendMail(details, (err) => {
+            if (err) {
+                return false;
+            } 
+            return true;
+        })
     }
 
     async resetPassword(data) {
