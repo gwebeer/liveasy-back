32a1072e8732e41cdd6acd0e5ee7f42ddb1a4e94
V-Perotto <vittorio.perotto@outlook.com>
2023-08-29 16:23:54 -0300

    Corrigindo retornos, funções e variáveis

--- src/controller/processController.js
+++ src/controller/processController.js
@@ -8,30 +8,30 @@ export default class Process {
             const createProcess = await processService.createProcess(request.body);
             return response.status(201).json(createProcess);
         } catch (error) {
-            return response.status(400).json({ error });
+            return response.status(400).json({ error: error.message });
         }
     }
     
     async getProcess(request, response) {
         try {
             const processService = new ProcessService();
-            const getProcesses = await processService.getProcess(request.body);
+            const getProcess = await processService.getProcess(request.body);
             if (getProcess == null) {
                 return response.status(404).json({ "message": "Não foi(ram) encontrado(s) o(s) processo(s).", "_return": getProcess });
             }
             return response.json(getProcess);
         } catch (error) {
-            return response.status(400).json({ error });
+            return response.status(400).json({ error: error.message });
         }
     }
     
     async updateProcess(request, response) {
         try {
             const processService = new ProcessService();
-            const updateProcess = await processModel.updateProcess(request.body);
+            const updateProcess = await processService.updateProcess(request.body);
             return response.status(200).json(updateProcess);
         } catch (error) {
-            return response.status(400).json({ error });
+            return response.status(400).json({ error: error.message });
         }
     }
     
@@ -44,7 +44,7 @@ export default class Process {
             }
             return response.status(200).json(deleteProcess);
         } catch (error) {
-            return response.status(400).json({ error });
+            return response.status(400).json({ error: error.message });
         }
     }
 
@@ -57,7 +57,7 @@ export default class Process {
             }
             return response.status(200).json(getUserProcess);
         } catch (error) {
-            return response.status(400).json({ error });
+            return response.status(400).json({ error: error.message });
         }
     }
 }
