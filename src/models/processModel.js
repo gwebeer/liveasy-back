d9af68ccca6cccc4f57effe19b5255faf086510f
V-Perotto <vittorio.perotto@outlook.com>
2023-08-29 16:25:14 -0300

    Adicionando novos atributos e corrigindo nome do banco de dados

--- src/models/processModel.js
+++ src/models/processModel.js
@@ -2,8 +2,11 @@ import { mongoose } from "mongoose";
 
 const ProcessSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    step: { type: String, required: true },
+    property: { type: mongoose.Schema.Types.ObjectId, ref: "Property", required: false },
     status: { type: String, required: true },    
+    income: { type: Number, required: true },
+    budget: { type: Number, required: true },
+    movingDate: { type: String, required: true }
 }, { timestamps: {} });
 
-export default mongoose.model('Moves', ProcessSchema);
+export default mongoose.model('Process', ProcessSchema); 
