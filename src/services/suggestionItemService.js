f870fe8500b1866c85616050733f9546fd96ddfb
V-Perotto <vittorio.perotto@outlook.com>
2023-08-29 15:41:17 -0300

    Corrigindo import e adicionando funcionalidades

--- src/services/suggestionItemService.js
+++ src/services/suggestionItemService.js
@@ -1,7 +1,38 @@
-import SuggestionItemController from '../controller/suggestionItemController.js';
+import SuggestionItemModel from '../models/suggestionItemModel.js';
 
 export default class SuggestionItemService {
 
+    async createSuggestionItem(data) {
+        const suggestionItemExists = await SuggestionItemModel.find({
+            title: data.title,
+            type: data.type
+        });
+        if (suggestionItemExists.length > 0) {
+            throw Error("Esse item de sugestão já existe.");
+        }
+        const createSuggestionItem = await SuggestionItemModel.create(data);
+        return createSuggestionItem;
+    }
 
+    async getSuggestionItem(data) {
+        if (data.id == "all") {
+            const getSuggestionItems = await SuggestionItem.find();
+            console.log("getSuggestionItems: ", getSuggestionItems);
+            return getSuggestionItems;
+        } else {
+            const getSuggestionItem = await SuggestionItemModel.findOne({ _id: data.id });
+            return getSuggestionItem;
+        } 
+    }
+
+    async updateSuggestionItem(data) {        
+        const updateSuggestionItem = await SuggestionItemModel.findOneAndUpdate({ _id: data.id }, data);
+        return updateSuggestionItem;
+    }
+
+    async deleteSuggestionItem(data) {        
+        const deleteSuggestionItem = await SuggestionItemModel.findOneAndDelete({ _id: data.id });
+        return deleteSuggestionItem;
+    }
     
 }
