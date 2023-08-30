503021bdc7139b1d229c5856788926a9f22f6e94
V-Perotto <vittorio.perotto@outlook.com>
2023-08-30 15:45:21 -0300

    Deletando Models/Controllers/Services/Routes de GiveUpProperty e ChoosedProperty

--- src/models/choosedPropertyModel.js
+++
@@ -1,9 +0,0 @@
-import { mongoose } from "mongoose";
-
-const ChoosedPropertySchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    property: { type: mongoose.Schema.Types.ObjectId, ref: "Property", required: true },
-    reason: { type: String, required: true }
-}, { timestamps: {} }); 
-
-export default mongoose.model("ChoosedProperty", ChoosedPropertySchema);
