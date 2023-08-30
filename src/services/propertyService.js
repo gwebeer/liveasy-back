3cb8729f14ba9e15193933f15a6de0e5aae6533b
V-Perotto <vittorio.perotto@outlook.com>
2023-08-30 14:48:54 -0300

    Corrigindo funcionalidades e instâncias

--- src/services/propertyService.js
+++ src/services/propertyService.js
@@ -2,34 +2,30 @@ import PropertyModel from '../models/propertyModel.js';
 
 export default class PropertyService {
 
-    async createProperty(data) {
-        const propertyModel = new PropertyModel(data);
-    
-        const propertyExists = await propertyModel.find({
+    async createProperty(data) {    
+        const propertyExists = await PropertyModel.find({
             name: data.name,
+            address: data.address,
             zipCode: data.zipCode
         });
-        if (propertyExists) {
+        if (propertyExists.length > 0) {
             throw Error("Essa propriedade já existe.");
         }
-
-        const createProperty = await propertyModel.create(data);
+        const createProperty = await PropertyModel.create(data);
         return createProperty; 
     }
 
     async getProperty(data) {
-        const propertyModel = new PropertyModel();
-
         if (data.id == "all") {
             try {
-                const getProperties = await propertyModel.find();
+                const getProperties = await PropertyModel.find();
                 return getProperties;
             } catch (error) {
                 throw Error("Houve um problema ao buscar as propriedades.");
             }
         } else {
             try {
-                const getProperty = await propertyModel.findOne({ _id: data.id });
+                const getProperty = await PropertyModel.findOne({ _id: data.id });
                 return getProperty;
             } catch (error) {
                 throw Error("Houve um problema ao buscar a propriedade.");
@@ -38,16 +34,12 @@ export default class PropertyService {
     }
 
     async updateProperty(data) {
-        const propertyModel = new PropertyModel(data);
-
-        const updateProperty = await propertyModel.findOneAndUpdate({ _id : data.id }, data);
+        const updateProperty = await PropertyModel.findOneAndUpdate({ _id : data.id }, data);
         return updateProperty;
     }
 
     async deleteProperty(data) {
-        const propertyModel = new PropertyModel(data);
-
-        const deleteProperty = await propertyModel.findOneAndDelete({ _id: data.id });
+        const deleteProperty = await PropertyModel.findOneAndDelete({ _id: data.id });
         return deleteProperty;
     }
 
