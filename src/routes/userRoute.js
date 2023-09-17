44db49b79dac73f1b2ec0b45df281e973f26c28a
V-Perotto <vittorio.perotto@outlook.com>
2023-09-17 16:39:38 -0300

    Modificando GET para receber parâmetros

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,7 +4,7 @@ import UserController from '../controller/userController.js';
 const routes = Router();
 
 routes.post('/user/register', (req, res) => new UserController().registerUser(req, res));  
-routes.get('/user', (req, res) => new UserController().getUser(req, res));             
+routes.get('/user/:id', (req, res) => new UserController().getUser(req, res));             
 routes.put('/user', (req, res) => new UserController().updateUser(req, res));          
 routes.delete('/user', (req, res) => new UserController().deleteUser(req, res));     
 
