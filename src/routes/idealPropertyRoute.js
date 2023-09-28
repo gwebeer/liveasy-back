46f0f6fbd1f0e22b9575a412484427e5cc951b89
V-Perotto <vittorio.perotto@outlook.com>
2023-09-28 18:03:05 -0300

    Corrigindo rota para user

--- src/routes/idealPropertyRoute.js
+++ src/routes/idealPropertyRoute.js
@@ -4,7 +4,7 @@ import IdealPropertyController from '../controller/idealPropertyController.js';
 const routes = Router();
 
 routes.post('/user/property/add', (req, res) => new IdealPropertyController().createIdealProperty(req, res));    
-routes.get('/user/property/:id', (req, res) => new IdealPropertyController().getIdealProperty(req, res));        
+routes.get('/user/property/:user', (req, res) => new IdealPropertyController().getIdealProperty(req, res));        
 routes.put('/user/property', (req, res) => new IdealPropertyController().updateIdealProperty(req, res));     
 routes.delete('/user/property/:id', (req, res) => new IdealPropertyController().deleteIdealProperty(req, res));  
 
