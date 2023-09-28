dc617593604b8cec7afcdaac3c47951b1e7c2a12
V-Perotto <vittorio.perotto@outlook.com>
2023-09-28 15:00:22 -0300

    Editando atributos

--- src/models/idealPropertyModel.js
+++ src/models/idealPropertyModel.js
@@ -2,15 +2,15 @@ import { mongoose } from "mongoose";
 
 const IdealPropertySchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    isForRent: { type: Boolean, required: true },
+    propertyType: { type: String, required: true },
     peopleLiving: { type: Number, required: true },
-    type: { type: String, required: true },
     rooms: { type: Number, required: true },
     bathrooms: { type: Number, required: true },
     parkingSpaces: { type: Number, required: true },
-    infraestructure: { type: Boolean, required: true },
+    infraestructure: { type: Array, required: true },
     furnished: { type: Boolean, required: true },
-    priceRange: { type: Number, required: true },
-    isForRent: { type: Boolean, required: true }
+    priceRange: { type: Number, required: false }
 }, { timestamps: {} });
 
 export default mongoose.model("IdealProperty", IdealPropertySchema);
