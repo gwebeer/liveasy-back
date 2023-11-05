1ea127ed80f55b315902d7fbcd6949ab6a13fb60
V-Perotto <vittorio.perotto@outlook.com>
2023-11-05 17:52:10 -0300

    Comentando costItemList

--- src/models/calendarReminderModel.js
+++ src/models/calendarReminderModel.js
@@ -2,7 +2,7 @@ import { mongoose } from "mongoose";
 
 const CalendarReminderSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    costItemList: { type: mongoose.Schema.Types.ObjectId, ref: "CostItemList", required: true },
+    // costItemList: { type: mongoose.Schema.Types.ObjectId, ref: "CostItemList", required: true },
     title: { type: String, required: true },
     start: { type: String, required: true },
     end: { type: String, required: false },
