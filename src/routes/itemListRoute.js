37eda4dbfc355d9b162cf24df8758c88b266e65f
V-Perotto <vittorio.perotto@outlook.com>
2023-11-05 16:05:40 -0300

    Adicionando Route para getItensNotBought

--- src/routes/itemListRoute.js
+++ src/routes/itemListRoute.js
@@ -8,4 +8,6 @@ routes.get('/user/list/item/:process', (req, res) => new ItemListController().ge
 routes.put('/user/list/item', (req, res) => new ItemListController().updateItemList(req, res));      
 routes.delete('/user/list/item/:id', (req, res) => new ItemListController().deleteItemList(req, res));   
 
+routes.get('/user/list/item/not-bought/:process', (req, res) => new ItemListController().getItensNotBought(req, res));
+
 export default routes;
