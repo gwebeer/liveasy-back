c21f0297907fb8a933d84eed2ec8e5aeba2bee8a
V-Perotto <vittorio.perotto@outlook.com>
2023-11-05 17:29:08 -0300

    Editando atributos da Model de CalendarReminder

--- src/models/calendarReminderModel.js
+++ src/models/calendarReminderModel.js
@@ -3,10 +3,10 @@ import { mongoose } from "mongoose";
 const CalendarReminderSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     costItemList: { type: mongoose.Schema.Types.ObjectId, ref: "CostItemList", required: true },
-    initialDate: { type: String, required: true },
-    finalDate: { type: String, required: false },
-    repeat: { type: Boolean, required: true },
-    repeatEvery: { type: Number, required: false}
+    title: { type: String, required: true },
+    start: { type: String, required: true },
+    end: { type: String, required: false },
+    allDay: { type: Boolean, required: true },
 }, { timestamps: {} });
 
 export default mongoose.model("CalendarReminder", CalendarReminderSchema);
