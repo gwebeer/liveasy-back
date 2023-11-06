bd519d23c3f981291464a2d707997b0d33ea841a
V-Perotto <vittorio.perotto@outlook.com>
2023-11-06 20:09:53 -0300

    Adicionando Route GET para todos os itens

--- src/routes/itemListRoute.js
+++ src/routes/itemListRoute.js
@@ -5,6 +5,7 @@ const routes = Router();
 
 routes.post('/user/list/item/add', (req, res) => new ItemListController().createItemList(req, res));     
 routes.get('/user/list/item/:process', (req, res) => new ItemListController().getItemList(req, res));         
+routes.get('/user/list/all/itens', (req, res) => new ItemListController().getAllItemList(req, res));         
 routes.put('/user/list/item', (req, res) => new ItemListController().updateItemList(req, res));      
 routes.delete('/user/list/item/:id', (req, res) => new ItemListController().deleteItemList(req, res));   
 
