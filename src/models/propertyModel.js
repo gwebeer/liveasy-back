29b142b2d479b229a8f08c259c033bc9c4b29b1e
gwebeer <weber_guilherme@outlook.com>
2023-11-08 14:41:41 -0300

    Correção Model Propertie

--- src/models/propertyModel.js
+++ src/models/propertyModel.js
@@ -2,22 +2,22 @@ import { mongoose } from "mongoose";
 
 const PropertySchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    isChoosed: { type: Boolean, required: false },    
-    isCondominium: { type: Boolean, required: true},
-    score: { type: Number, required: false },
+    isChoosed: { type: String, required: false },    
+    isCondominium: { type: String, required: true},
+    score: { type: String, required: false },
     name: { type: String, required: true },
     address: { type: String, required: false },
     zipCode: { type: String, required: false },
     neighborhood: { type: String, required: false },
     type: { type: String, required: true },
-    rooms: { type: Number, required: true },
-    bathrooms: { type: Number, required: true },
-    parkingSpaces: { type: Number, required: true },
-    infraestructure: { type: Boolean, required: true },
-    furnished: { type: Boolean, required: true },
+    rooms: { type: String, required: true },
+    bathrooms: { type: String, required: true },
+    parkingSpaces: { type: String, required: true },
+    infraestructure: [{ type: String, required: true }],
+    furnished: { type: String, required: true },
     furniture: [{ type: String, required: false }],
-    isForRent: { type: Boolean, required: true },
-    value: { type: Number, required: true }
+    isForRent: { type: String, required: true },
+    value: { type: String, required: true }
 }, { timestamps: {} });
 
 export default mongoose.model("Property", PropertySchema);
